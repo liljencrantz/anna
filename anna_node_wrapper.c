b1a36f62ca369bd7e6672f07ce32b0137028587c
Axel Liljencrantz <axel@kendra.(none)>
2010-03-29 16:45:36 +0200

    Call.__each__()

--- anna_node_wrapper.c
+++ anna_node_wrapper.c
@@ -379,6 +379,40 @@ static anna_object_t *anna_node_call_wrapper_i_init(anna_object_t **param)
     return param[0];
 }
 
+static anna_object_t *anna_node_call_wrapper_i_each(anna_object_t **param)
+{
+    if(param[1]==null_object)
+	return null_object;
+
+    anna_node_call_t *node = (anna_node_call_t *)anna_node_unwrap(param[0]);
+    size_t i;
+
+    anna_object_t *result=null_object;
+    anna_object_t *body_object;
+    body_object=param[1];
+
+    anna_function_t **function_ptr = (anna_function_t **)anna_member_addr_get_mid(body_object, ANNA_MID_FUNCTION_WRAPPER_PAYLOAD);
+    anna_stack_frame_t **stack_ptr = (anna_stack_frame_t **)anna_member_addr_get_mid(body_object, ANNA_MID_FUNCTION_WRAPPER_STACK);
+    anna_stack_frame_t *stack = stack_ptr?*stack_ptr:0;
+    assert(function_ptr);
+/*
+  wprintf(L"each loop got function %ls\n", (*function_ptr)->name);
+  wprintf(L"with param %ls\n", (*function_ptr)->input_name[0]);
+*/  
+    anna_object_t *o_param[2];
+    for(i=0;i<node->child_count;i++)
+    {
+	/*
+	  wprintf(L"Run the following code:\n");
+	  anna_node_print((*function_ptr)->body);
+	  wprintf(L"\n");
+	*/
+	o_param[0] = anna_int_create(i);
+	o_param[1] = anna_node_wrap(node->child[i]);
+	result = anna_function_invoke_values(*function_ptr, 0, o_param, stack);
+    }
+    return result;
+}
 
 void anna_node_call_wrapper_type_create(anna_stack_frame_t *stack)
 {
@@ -440,6 +474,33 @@ void anna_node_call_wrapper_type_create(anna_stack_frame_t *stack)
 	}
     ;
     
+    anna_node_t *e_method_argv[] = 
+	{
+	    (anna_node_t *)anna_node_identifier_create(0, L"Int"),
+	    (anna_node_t *)anna_node_identifier_create(0, L"Node")
+	}
+    ;
+
+    wchar_t *e_method_argn[] = 
+	{
+	    L"index",
+	    L"value"
+	}
+    ;
+
+    anna_node_t *e_argv[] = 
+	{
+	    (anna_node_t *)anna_node_identifier_create(0, L"Call"),
+	    anna_node_function_declaration_create(0, (anna_node_t *)anna_node_identifier_create(0, L"Node"), 2, e_method_argv, e_method_argn)
+	}
+    ;
+
+    wchar_t *e_argn[]=
+	{
+	    L"this", L"block"
+	}
+    ;
+    
     node_call_wrapper_type = anna_type_native_create(L"Call", stack);
     anna_type_native_parent(node_call_wrapper_type, L"Node");
     
@@ -454,6 +515,12 @@ void anna_node_call_wrapper_type_create(anna_stack_frame_t *stack)
 	(anna_node_t *)anna_node_identifier_create(0, L"Null") , 
 	4, argv, argn);
     
+    anna_native_method_add_node(
+	definition, -1, L"__each__", 0, 
+	(anna_native_t)&anna_node_call_wrapper_i_each, 
+	(anna_node_t *)anna_node_identifier_create(0, L"Node"), 
+	2, e_argv, e_argn);
+  
     anna_native_method_add_node(
 	definition, -1, L"getCount", 0, 
 	(anna_native_t)&anna_node_call_wrapper_i_get_count, 
