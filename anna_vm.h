8a194872637b0269e3dfdcfe9a5e933db1d49ba0
Axel Liljencrantz <axel@liljencrantz.se>
2011-03-28 00:59:03 +0200

    Planning on how to support continuations and native functions

--- anna_vm.h
+++ anna_vm.h
@@ -1,6 +1,8 @@
 #ifndef ANNA_VM_H
 #define ANNA_VM_H
 
+typedef anna_object_t **(*anna_vm_callback_t)(void *aux);
+
 void anna_vm_compile(
     anna_function_t *fun);
 
@@ -8,6 +10,10 @@ void anna_bc_print(char *code);
 
 void anna_vm_init(void);
 anna_object_t *anna_vm_run(anna_object_t *entry, int argc, anna_object_t **argv);
+
+void anna_vm_call_loop(anna_vm_callback_t callback, void *aux, anna_object_t *entry, int argc);
+void anna_vm_call_once(anna_object_t *entry, int argc, anna_object_t **argv);
+
 size_t anna_vm_stack_frame_count();
 anna_vmstack_t *anna_vm_stack_get(size_t idx);
 void anna_vm_mark_code(anna_function_t *f);
