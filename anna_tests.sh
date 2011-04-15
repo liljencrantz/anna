31a36dca5c9f7924534fed595f7b64c24e829c90
Axel Liljencrantz <axel@liljencrantz.se>
2011-04-15 11:53:00 +0200

    Slightly better error checker

--- anna_tests.sh
+++ anna_tests.sh
@@ -1,23 +1,35 @@
 #! /bin/bash	
 
+error_count=0
+test_count=0
 for i in tests/*.anna; do
-    echo "Run test $i"
+#    echo "Run test $i"
     ./anna tests/$(basename $i .anna) >anna_tests.out 2>/dev/null
     status=$?
     out_correct=tests/$(basename $i .anna).output
-    status_correct=tests/$(basename $i .anna).status
+    status_correct_file=tests/$(basename $i .anna).status
+    status_correct=0
+    error=0
     if test -f $out_correct; then
 	if diff anna_tests.out $out_correct; then
 	    true
 	else
+	    error=1
 	    echo "Error in output for test $i!!"
 	fi
     fi
-    if test -f $status_correct; then
-	if test $(cat $status_correct) = $status; then
-	    true
-	else
-	    echo "Error in exit status for test $i." $(cat $status_correct) != $status
-	fi
+    if test -f $status_correct_file; then
+	status_correct=$(cat $status_correct_file)
     fi
+
+    if test "$status" != "$status_correct"; then
+	echo "Error in exit status for test $i." $status_correct != $status
+	error=1
+    fi
+    
+    error_count=$(echo $error_count + $error|bc)
+    test_count=$(echo $test_count + 1|bc)
+    
 done
+
+echo "Found $error_count errors while running $test_count tests"
