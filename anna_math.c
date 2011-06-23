c99763fcd3d180fbc37121f7c63d69ab222097d1
Axel Liljencrantz <axel@liljencrantz.se>
2011-06-24 01:35:53 +0200

    Arc sin/cos/tan

--- anna_math.c
+++ anna_math.c
@@ -37,6 +37,26 @@ ANNA_NATIVE(anna_math_tan, 1)
     return anna_entry_null(param[0])? null_entry : anna_from_float(tan(anna_as_float(param[0])));
 }
 
+ANNA_NATIVE(anna_math_asin, 1)
+{
+    return anna_entry_null(param[0])? null_entry : anna_from_float(asin(anna_as_float(param[0])));
+}
+
+ANNA_NATIVE(anna_math_acos, 1)
+{
+    return anna_entry_null(param[0])? null_entry : anna_from_float(acos(anna_as_float(param[0])));
+}
+
+ANNA_NATIVE(anna_math_atan, 1)
+{
+    return anna_entry_null(param[0])? null_entry : anna_from_float(atan(anna_as_float(param[0])));
+}
+
+ANNA_NATIVE(anna_math_log, 1)
+{
+    return anna_entry_null(param[0])? null_entry : anna_from_float(log(anna_as_float(param[0])));
+}
+
 void anna_math_load(anna_stack_template_t *stack)
 {
     anna_module_const_float(stack, L"pi", M_PI);
@@ -50,21 +70,48 @@ void anna_math_load(anna_stack_template_t *stack)
 	0, &anna_math_sin, 
 	float_type, 
 	1, f_argv, f_argn, 
-	L"The sin function comutes the sine of value, where value is given in radians.");
+	L"The sin function computes the sine of value, where value is given in radians.");
     
     anna_module_function(
 	stack, L"cos", 
 	0, &anna_math_cos, 
 	float_type, 
 	1, f_argv, f_argn, 
-	L"The cos function comutes the cosine of value, where value is given in radians.");
+	L"The cos function computes the cosine of value, where value is given in radians.");
     
     anna_module_function(
 	stack, L"tan", 
 	0, &anna_math_tan, 
 	float_type, 
 	1, f_argv, f_argn, 
-	L"The tan function comutes the tangent of value, where value is given in radians.");
+	L"The tan function computes the tangent of value, where value is given in radians.");
     
+    anna_module_function(
+	stack, L"asin", 
+	0, &anna_math_asin, 
+	float_type, 
+	1, f_argv, f_argn, 
+	L"The tan function computes the arc sine of value.");
+    
+    anna_module_function(
+	stack, L"acos", 
+	0, &anna_math_acos, 
+	float_type, 
+	1, f_argv, f_argn, 
+	L"The tan function computes the arc cosine of value.");
+    
+    anna_module_function(
+	stack, L"atan", 
+	0, &anna_math_atan, 
+	float_type,
+	1, f_argv, f_argn, 
+	L"The atan function computes the arc tangent of value.");
+    
+    anna_module_function(
+	stack, L"log", 
+	0, &anna_math_log, 
+	float_type,
+	1, f_argv, f_argn, 
+	L"The log function computes the natural logarithm of value.");
 }
 
