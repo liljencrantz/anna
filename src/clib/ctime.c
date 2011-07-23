66b8bbdf2681225f3988aa6dd8402205706e9386
Axel Liljencrantz <axel@liljencrantz.se>
2011-07-23 13:00:28 +0200

    Documentation

--- src/clib/ctime.c
+++ src/clib/ctime.c
@@ -10,6 +10,7 @@
 #include "wutil.h"
 #include "anna.h"
 #include "anna_module.h"
+#include "anna_module_data.h"
 #include "anna_vm.h"
 #include "anna_member.h"
 #include "anna_mid.h"
@@ -51,6 +52,7 @@ ANNA_VM_NATIVE(anna_ctime_get_timezone_name, 1)
 	free(wtz);
 	return anna_from_obj(res);
     }
+
     FILE *fp = fopen("/etc/timezone", "r");
     char buff[CTIME_BUFF_SZ];
     if(fp)
@@ -206,28 +208,84 @@ ANNA_VM_NATIVE(anna_ctime_break_time, 2)
 	unsetenv("TZ");
     tzset();
     free(ntz);
-
+    
     return res ? handle_tm(&tm) : null_entry;
+}
+
+static void anna_ctime_broken_load(anna_stack_template_t *stack)
+{
+    anna_module_const_int(stack, L"second", 0, 0);
+    anna_module_const_int(stack, L"minute", 1, 0);
+    anna_module_const_int(stack, L"hour", 2, 0);
+    anna_module_const_int(stack, L"dayOfMonth", 3, 0);
+    anna_module_const_int(stack, L"month", 4, 0);
+    anna_module_const_int(stack, L"year", 5, 0);
+    anna_module_const_int(stack, L"dayOfWeek", 6, 0);
+    anna_module_const_int(stack, L"dayOfYear", 7, 0);
+    anna_module_const_int(stack, L"isDaylightSaving", 8, 0);
 }
 
 void anna_ctime_load(anna_stack_template_t *stack)
 {
+    /*
+      A simple, low level time API. Unlike most other c* api:s in
+      Anna, this one doesn't mimic the underlying C
+      functions. Specifically, it is rather different in how timezones
+      are handled, as a timezone name can be passed as an argument to
+      various functions.
+      
+      Internally, timezone handling is handled using an extremely ugly
+      hack: The TZ environment variable is set, and the tzset function
+      is called. This temporarily changes the timezone information of
+      the running application, something which is obviously not thread
+      safe. Unfortunatly, this seems to be the only reliable way to
+      perform timezone calculations using the C API.
+      
+      This library deals with time in two different formats, time
+      stamps and broken down time. 
+      
+      A time stamp counts the number of whole seconds that have passed
+      since the Epoch (midnight, January 1, 1970, UTC). A time stamp
+      is never relative to any timezone other than UTC. The
+      getTimeOfDay function returns a time stamp plus an additional
+      microsecond fraction of time.
+      
+      Broken down time a time separated into fields representing the
+      number of seconds past the minute (0..60), minutes past the hour
+      (0..59), hours after midnight (0..24), day of the month (1..31),
+      months since january (0..11), years after 1900, days since
+      sunday (0..6), days since january 1 (0..365) and finally a field
+      that is set to 1 if daylight saving time is in effect, 0
+      otherwise. Broken down time is always relative to some specific
+      time zone.
+    */
 
+    anna_module_data_t modules[] = 
+	{
+	    { L"broken", 0, anna_ctime_broken_load },
+	};
+
+    anna_module_data_create(modules, stack);
+    
     anna_module_function(
 	stack, L"getTimeOfDay", 
 	0, &anna_ctime_gettimeofday, 
 	anna_list_type_get_imutable(int_type), 
 	0, 0, 0,
-	L"Returns the current time of day as the number of seconds and microseconds since the Epoch (midnight, January 1, 1970, UTC). Wrapper for the C gettimeofday function, see man 2 gettimeofday. Because the timezone argument of gettimeofday doesn't reliably work, it's not supported. See the timezoneName and timezoneOffset properties for actually working timezone information.");
+	L"Returns the current time of day as the number of seconds and microseconds since the Epoch (midnight, January 1, 1970, UTC). Wrapper for the C gettimeofday function, see man 2 gettimeofday. Because the timezone argument of gettimeofday doesn't reliably work, it's not supported. Use the timezoneName and timezoneOffset properties for working timezone information.");
     
     anna_type_t *mt_argv[] = 
 	{
-	    int_type, int_type, int_type, int_type, int_type, int_type, int_type, string_type
+	    int_type, int_type, int_type, 
+	    int_type, int_type, int_type, 
+	    int_type, string_type
 	}
     ;
     wchar_t *mt_argn[] = 
 	{
-	    L"sec", L"min", L"hour", L"mday", L"month", L"year", L"isDaylightSaving", L"timezone"
+	    L"sec", L"min", L"hour", 
+	    L"mday", L"month", L"year", 
+	    L"isDaylightSaving", L"timezone"
 	}
     ;
 
@@ -236,7 +294,7 @@ void anna_ctime_load(anna_stack_template_t *stack)
 	0, &anna_ctime_mktime, 
 	int_type, 
 	8, mt_argv, mt_argn,
-	L"Convert a time list to a time stamp");
+	L"Convert a broken down time to a time stamp");
     
     anna_type_t *a_argv[] = 
 	{
@@ -254,7 +312,7 @@ void anna_ctime_load(anna_stack_template_t *stack)
 	0, &anna_ctime_break_time, 
 	anna_list_type_get_imutable(int_type), 
 	2, a_argv, a_argn,
-	L"Convert a time stamp to a list of date data in the specified timezone. If no timezone is specified, this function is equivalent to the localtime C function.");
+	L"Convert a time stamp to broken down time in the specified timezone. If no timezone is specified, this function is equivalent to the localtime C function.");
     
     anna_type_t *type = anna_stack_wrap(stack)->type;
 
@@ -272,4 +330,3 @@ void anna_ctime_load(anna_stack_template_t *stack)
 	&anna_ctime_set_timezone_name,
 	L"The name of the currently configured timezone.");
 }
-
