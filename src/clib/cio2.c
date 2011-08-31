2526e3b504b4b89cacf395512c83a6747355c57d
Axel Liljencrantz <axel@liljencrantz.se>
2011-09-01 00:04:20 +0200

    Bindings are starting to shape up, replacement version of cio is now complete.

--- src/clib/cio2.c
+++ src/clib/cio2.c
@@ -22,6 +22,7 @@
 #include "clib/lang/string.h"
 #include "clib/lang/buffer.h"
 #include "anna_member.h"
+#include "anna_module_data.h"
 
 // Declare internal variables for all types defined in this module
 anna_type_t *cio2_stat_type;
@@ -130,6 +131,71 @@ ANNA_VM_NATIVE(cio2_i_stat_ctime_getter, 1)
     struct stat *data = (struct stat *)anna_entry_get(anna_as_obj_fast(param[0]), ANNA_MID_CSTRUCT_PAYLOAD);
     anna_entry_t *result = anna_from_int(data->st_ctime);
     return result;
+}
+
+void anna_open_mode_load(anna_stack_template_t *stack)
+{
+    anna_module_data_t modules[] =
+        {
+        };
+    anna_module_data_create(modules, stack);
+
+    wchar_t *this_argn[] = {L"this"};
+
+    anna_module_const_int(stack, L"readOnly", O_RDONLY, L"");
+    anna_module_const_int(stack, L"writeOnly", O_WRONLY, L"");
+    anna_module_const_int(stack, L"readWrite", O_RDWR, L"");
+    anna_module_const_int(stack, L"append", O_APPEND, L"");
+    anna_module_const_int(stack, L"async", O_ASYNC, L"");
+    anna_module_const_int(stack, L"create", O_CREAT, L"");
+    anna_module_const_int(stack, L"closeOnExec", O_CLOEXEC, L"");
+    anna_module_const_int(stack, L"direct", O_DIRECT, L"");
+    anna_module_const_int(stack, L"directory", O_DIRECTORY, L"");
+    anna_module_const_int(stack, L"exclusive", O_EXCL, L"");
+    anna_module_const_int(stack, L"largeFile", O_LARGEFILE, L"");
+    anna_module_const_int(stack, L"noAccessTime", O_NOATIME, L"");
+    anna_module_const_int(stack, L"noControllingTTY", O_NOCTTY, L"");
+    anna_module_const_int(stack, L"noFollow", O_NOFOLLOW, L"");
+    anna_module_const_int(stack, L"nonBlock", O_NONBLOCK, L"");
+    anna_module_const_int(stack, L"synchronous", O_SYNC, L"");
+    anna_module_const_int(stack, L"truncate", O_TRUNC, L"");
+
+
+}
+
+void anna_stat_mode_load(anna_stack_template_t *stack)
+{
+    anna_module_data_t modules[] =
+        {
+        };
+    anna_module_data_create(modules, stack);
+
+    wchar_t *this_argn[] = {L"this"};
+
+    anna_module_const_int(stack, L"regular", S_IFREG, L"");
+    anna_module_const_int(stack, L"socket", S_IFSOCK, L"");
+    anna_module_const_int(stack, L"link", S_IFLNK, L"");
+    anna_module_const_int(stack, L"block", S_IFBLK, L"");
+    anna_module_const_int(stack, L"directory", S_IFDIR, L"");
+    anna_module_const_int(stack, L"character", S_IFCHR, L"");
+    anna_module_const_int(stack, L"fifo", S_IFIFO, L"");
+    anna_module_const_int(stack, L"suid", S_ISUID, L"");
+    anna_module_const_int(stack, L"sgid", S_ISGID, L"");
+    anna_module_const_int(stack, L"sticky", S_ISVTX, L"");
+    anna_module_const_int(stack, L"userAll", S_IRWXU, L"");
+    anna_module_const_int(stack, L"userRead", S_IRUSR, L"");
+    anna_module_const_int(stack, L"userwrite", S_IWUSR, L"");
+    anna_module_const_int(stack, L"userExecute", S_IXUSR, L"");
+    anna_module_const_int(stack, L"groupAll", S_IRWXG, L"");
+    anna_module_const_int(stack, L"groupRead", S_IRGRP, L"");
+    anna_module_const_int(stack, L"groupwrite", S_IWGRP, L"");
+    anna_module_const_int(stack, L"groupExecute", S_IXGRP, L"");
+    anna_module_const_int(stack, L"otherAll", S_IRWXO, L"");
+    anna_module_const_int(stack, L"otherRead", S_IROTH, L"");
+    anna_module_const_int(stack, L"otherwrite", S_IWOTH, L"");
+    anna_module_const_int(stack, L"otherExecute", S_IXOTH, L"");
+
+
 }
 
 ANNA_VM_NATIVE(cio2_i_open, 3)
@@ -317,6 +383,65 @@ ANNA_VM_NATIVE(cio2_i_unlink, 1)
     return result;
 }
 
+ANNA_VM_NATIVE(cio2_i_getcwd, 2)
+{
+    // Mangle input parameters
+    if(param[0] == null_entry){return null_entry;}
+    unsigned char *native_param_buf = anna_buffer_get_payload(anna_as_obj(param[0]));
+    if(param[1] == null_entry){return null_entry;}
+    int native_param_size = anna_as_int(param[1]);
+
+    // Validate parameters
+    if(anna_buffer_ensure_capacity(anna_as_obj(param[0]), native_param_size)) 
+    {
+        return null_entry;
+    }
+    else
+    {
+        native_param_buf = anna_buffer_get_payload(anna_as_obj(param[0]));
+    }
+
+    // Call the function
+    anna_entry_t *result = anna_from_int(getcwd(native_param_buf, native_param_size));
+    // Perform cleanup
+
+    // Return result
+    return result;
+}
+
+ANNA_VM_NATIVE(cio2_i_chdir, 1)
+{
+    // Mangle input parameters
+    if(param[0] == null_entry){return null_entry;}
+    char *native_param_path = anna_string_payload_narrow(anna_as_obj(param[0]));
+
+    // Validate parameters
+
+    // Call the function
+    anna_entry_t *result = anna_from_int(chdir(native_param_path));
+    // Perform cleanup
+    free(native_param_path);
+
+    // Return result
+    return result;
+}
+
+ANNA_VM_NATIVE(cio2_i_fchdir, 1)
+{
+    // Mangle input parameters
+    if(param[0] == null_entry){return null_entry;}
+    int native_param_fd = anna_as_int(param[0]);
+
+    // Validate parameters
+
+    // Call the function
+    anna_entry_t *result = anna_from_int(fchdir(native_param_fd));
+    // Perform cleanup
+
+    // Return result
+    return result;
+}
+
 
 // This function is called to create all types defined in this module
 void create(anna_stack_template_t *stack)
@@ -325,11 +450,18 @@ void create(anna_stack_template_t *stack)
 }
 
 // This function is called to load all functions and other declarations into the module
+
 void load(anna_stack_template_t *stack)
 {
+    anna_module_data_t modules[] =
+        {
+            { L"openMode", 0, anna_open_mode_load},
+            { L"statMode", 0, anna_stat_mode_load},
+        };
+    anna_module_data_create(modules, stack);
+
     wchar_t *this_argn[] = {L"this"};
 
-    anna_type_t *stack_type = anna_stack_wrap(stack)->type;
 
     anna_member_create_blob(cio2_stat_type, ANNA_MID_CSTRUCT_PAYLOAD, 0, sizeof(struct stat));
 
@@ -424,6 +556,22 @@ void load(anna_stack_template_t *stack)
     anna_type_t *cio2_i_unlink_argv[] = {string_type};
     wchar_t *cio2_i_unlink_argn[] = {L"path"};
     anna_module_function(stack, L"unlink", 0, &cio2_i_unlink, int_type, 1, cio2_i_unlink_argv, cio2_i_unlink_argn, L"");
+    anna_module_const_int(stack, L"standardInput", 0, L"");
+    anna_module_const_int(stack, L"standardOutput", 1, L"");
+    anna_module_const_int(stack, L"standardError", 2, L"");
+
+    anna_type_t *cio2_i_getcwd_argv[] = {buffer_type, int_type};
+    wchar_t *cio2_i_getcwd_argn[] = {L"buf", L"size"};
+    anna_module_function(stack, L"getcwd", 0, &cio2_i_getcwd, int_type, 2, cio2_i_getcwd_argv, cio2_i_getcwd_argn, L"");
+
+    anna_type_t *cio2_i_chdir_argv[] = {string_type};
+    wchar_t *cio2_i_chdir_argn[] = {L"path"};
+    anna_module_function(stack, L"chdir", 0, &cio2_i_chdir, int_type, 1, cio2_i_chdir_argv, cio2_i_chdir_argn, L"");
+
+    anna_type_t *cio2_i_fchdir_argv[] = {int_type};
+    wchar_t *cio2_i_fchdir_argn[] = {L"fd"};
+    anna_module_function(stack, L"fchdir", 0, &cio2_i_fchdir, int_type, 1, cio2_i_fchdir_argv, cio2_i_fchdir_argn, L"");
 
     anna_type_data_register(anna_type_data, stack);
 }
+
