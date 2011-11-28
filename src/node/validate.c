65ee7ee4fdd371b87a515ac9c2351ce6ef1caad1
Axel Liljencrantz <axel@liljencrantz.se>
2011-11-28 10:32:55 +0100

    Add validation for assigning statically to non-static members. Ad three static members related test cases

--- src/node/validate.c
+++ src/node/validate.c
@@ -158,16 +158,28 @@ void anna_node_validate(anna_node_t *this, anna_stack_template_t *stack)
 	    anna_node_member_access_t *c = (anna_node_member_access_t *)this;
 	    anna_type_t * type = 
 		c->object->return_type;
-	    if(c->access_type == ANNA_NODE_ACCESS_STATIC_MEMBER)
-	    {
-		type = anna_node_resolve_to_type(c->object, stack);
-	    }
 	    anna_member_t *memb = anna_member_get(type, c->mid);
 	    if(anna_member_is_property(memb) && memb->setter_offset == -1)
 	    {
 		anna_error(this, L"No setter for property %ls", anna_mid_get_reverse(c->mid));
 		break;		
 	    }	    
+	    break;
+	}
+	
+	case ANNA_NODE_STATIC_MEMBER_SET:
+	{
+	    anna_node_member_access_t *c = (anna_node_member_access_t *)this;
+	    anna_type_t * type = 
+		 anna_node_resolve_to_type(c->object, stack);
+	    
+	    anna_member_t *memb = anna_member_get(type, c->mid);
+
+	    if(!(memb->storage & ANNA_MEMBER_STATIC))
+	    {
+		anna_error(this, L"Tried to assign to non-static member statically");
+	    }
+
 	    break;
 	}
 	
