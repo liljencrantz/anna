6ec878be4ab5d508d3a9a6314ef6174e33d1d52e
Axel Liljencrantz <axel@liljencrantz.se>
2013-06-03 00:15:12 +0200

    Make pythons sieve implementation more pythonic

--- perf/sieve.py
+++ perf/sieve.py
@@ -1,14 +1,15 @@
 # Naive implementation of the
 # Sieve of Eratosthenes
+import sys
 
-end = 1000000;
-sieve = [None] * end;
-count = 0;
+end = int(sys.argv[1])
+sieve = [None] * end
+count = 0
 
 for i in xrange(2, end):
     if sieve[i] is None:
 	for j in xrange(2*i, end, i):
-            sieve[j] = 1;
-        count+= 1;
+            sieve[j] = 1
+        count+= 1
 
 print "There are", count, "prime numbers under", end
